2408f1a162e1a39e303fc248eed65fb40bf9f140
Mikko Rinne <mikko.rinne@aalto.fi>
2016-11-11 16:48:48 +0200

    Added stackoverflow attribution to HashUtil.

--- app/src/main/java/fi/aalto/trafficsense/trafficsense/util/HashUtil.java
+++ app/src/main/java/fi/aalto/trafficsense/trafficsense/util/HashUtil.java
@@ -1,6 +1,5 @@
 package fi.aalto.trafficsense.trafficsense.util;
 
-
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -9,6 +8,7 @@ public class HashUtil {
     final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     /** Convert Byte array to Hex (string) */
+    // Originates from https://stackoverflow.com/a/9855338
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for ( int j = 0; j < bytes.length; j++ ) {
