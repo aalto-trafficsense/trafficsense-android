f44fb21be711f23b19aeaeca39f12667f4618883
Mikko Rinne <mikko.rinne@aalto.fi>
2016-12-16 16:42:52 +0200

    Broadcast and survey notification separation and Finnish localisation added.

---
+++ app/src/main/java/fi/aalto/trafficsense/trafficsense/backend/backend_util/ServerNotification.java
@@ -0,0 +1,11 @@
+package fi.aalto.trafficsense.trafficsense.backend.backend_util;
+
+/**
+ * Created by mikko.rinne@aalto.fi on 16/12/16.
+ *
+ * Hold notifications coming from Firebase messaging
+ */
+public class TSNotification {
+
+
+}
