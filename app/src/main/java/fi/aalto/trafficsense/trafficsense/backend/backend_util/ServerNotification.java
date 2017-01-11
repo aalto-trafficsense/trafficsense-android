aab1567894a5606b4657dd77814aaf492db34896
Mikko Rinne <mikko.rinne@aalto.fi>
2017-01-11 15:19:52 +0200

    Ptp alert end time handling implemented.

--- app/src/main/java/fi/aalto/trafficsense/trafficsense/backend/backend_util/ServerNotification.java
+++ app/src/main/java/fi/aalto/trafficsense/trafficsense/backend/backend_util/ServerNotification.java
@@ -1,5 +1,6 @@
 package fi.aalto.trafficsense.trafficsense.backend.backend_util;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -14,10 +15,17 @@ import fi.aalto.trafficsense.trafficsense.R;
 import fi.aalto.trafficsense.trafficsense.TrafficSenseApplication;
 import fi.aalto.trafficsense.trafficsense.ui.MainActivity;
 import fi.aalto.trafficsense.trafficsense.util.EnvInfo;
+import fi.aalto.trafficsense.trafficsense.util.TSBootReceiver;
 import timber.log.Timber;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
+
+import static android.app.AlarmManager.RTC;
 
 /**
  * Created by mikko.rinne@aalto.fi on 16/12/16.
@@ -29,7 +37,7 @@ public class ServerNotification {
     private String uriString;
     private String messageTitle;
     private String messageBody;
-    private int notificationType;
+    private int notificationType = NOTIFICATION_TYPE_UNKNOWN;
     private boolean shouldCreateNotification = true;
 
 
@@ -45,12 +53,27 @@ public class ServerNotification {
     public static final String KEY_NOTIFICATION_MESSAGE_FI = "NOTIFICATION_MESSAGE_FI";
     public static final String KEY_NOTIFICATION_URI_FI = "NOTIFICATION_URI_FI";
 
-    private static final int SERVER_NOTIFICATION_ID = 1213;
+    public static final int SERVER_NOTIFICATION_ID = 1213;
 
+    public static final String PTP_ALERT_END_ACTION = "fi.aalto.trafficsense.action.PTP_ALERT_END";
+
+    public static final int NOTIFICATION_TYPE_UNKNOWN = 0;
     public static final int NOTIFICATION_TYPE_SURVEY = 1;
     public static final int NOTIFICATION_TYPE_BROADCASTMESSAGE = 2;
+    public static final int NOTIFICATION_TYPE_FERRY = 3;
+    public static final int NOTIFICATION_TYPE_SUBWAY = 4;
+    public static final int NOTIFICATION_TYPE_TRAIN = 5;
+    public static final int NOTIFICATION_TYPE_TRAM = 6;
+    public static final int NOTIFICATION_TYPE_BUS = 7;
+
+    private static final String PTP_ALERT_PUBTRANS = "PTP_ALERT_PUBTRANS";
+    private static final String PTP_ALERT_END = "PTP_ALERT_END";
+    private static final String PTP_ALERT_TYPE = "PTP_ALERT_TYPE";
 
     public static final String SURVEY_PREFS_FILE_NAME = "SurveyPrefs";
+
+    private SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
 
     public ServerNotification(String mUri, String mTitle, String mBody) {
         uriString = mUri;
@@ -66,42 +89,42 @@ public class ServerNotification {
             Timber.d("From: %s", from);
             Map<String, String> msgPayload = remoteMessage.getData();
 
-            if (from.startsWith(FB_TOPIC_PREFIX)) { // Broadcast topic message
+            // All messages currently may have a title, message and uri.
+            if (msgPayload.containsKey(KEY_NOTIFICATION_TITLE)) {
+                messageTitle = msgPayload.get(KEY_NOTIFICATION_TITLE);
+            } else {
+                shouldCreateNotification = false;
+            }
+            if (msgPayload.containsKey(KEY_NOTIFICATION_MESSAGE)) {
+                messageBody = msgPayload.get(KEY_NOTIFICATION_MESSAGE);
+            } else {
+                shouldCreateNotification = false;
+            }
+            if (msgPayload.containsKey(KEY_NOTIFICATION_URI)) {
+                uriString = msgPayload.get(KEY_NOTIFICATION_URI);
+            } else {
+                uriString = "";
+            }
 
-                // All broadcast notifications currently have a title, message and uri.
-                if (msgPayload.containsKey(KEY_NOTIFICATION_TITLE)) {
-                    messageTitle = msgPayload.get(KEY_NOTIFICATION_TITLE);
-                } else {
-                    shouldCreateNotification = false;
+            // If terminal locale is set to Finnish and there is Finnish content => replace
+            String loc = Locale.getDefault().getLanguage();
+            // Timber.d("ServerNotification got language as: %s", loc);
+            if (loc.equalsIgnoreCase("fi")) {
+                if (msgPayload.containsKey(KEY_NOTIFICATION_TITLE_FI)) {
+                    messageTitle = msgPayload.get(KEY_NOTIFICATION_TITLE_FI);
                 }
-                if (msgPayload.containsKey(KEY_NOTIFICATION_MESSAGE)) {
-                    messageBody = msgPayload.get(KEY_NOTIFICATION_MESSAGE);
-                } else {
-                    shouldCreateNotification = false;
+                if (msgPayload.containsKey(KEY_NOTIFICATION_MESSAGE_FI)) {
+                    messageBody = msgPayload.get(KEY_NOTIFICATION_MESSAGE_FI);
                 }
-                if (msgPayload.containsKey(KEY_NOTIFICATION_URI)) {
-                    uriString = msgPayload.get(KEY_NOTIFICATION_URI);
+                if (msgPayload.containsKey(KEY_NOTIFICATION_URI_FI)) {
+                    uriString = msgPayload.get(KEY_NOTIFICATION_URI_FI);
                 } else {
                     uriString = "";
                 }
 
-                // If terminal locale is set to Finnish and there is Finnish content => replace
-                String loc = Locale.getDefault().getLanguage();
-                // Timber.d("ServerNotification got language as: %s", loc);
-                if (loc.equalsIgnoreCase("fi")) {
-                    if (msgPayload.containsKey(KEY_NOTIFICATION_TITLE_FI)) {
-                        messageTitle = msgPayload.get(KEY_NOTIFICATION_TITLE_FI);
-                    }
-                    if (msgPayload.containsKey(KEY_NOTIFICATION_MESSAGE_FI)) {
-                        messageBody = msgPayload.get(KEY_NOTIFICATION_MESSAGE_FI);
-                    }
-                    if (msgPayload.containsKey(KEY_NOTIFICATION_URI_FI)) {
-                        uriString = msgPayload.get(KEY_NOTIFICATION_URI_FI);
-                    } else {
-                        uriString = "";
-                    }
+            }
 
-                }
+            if (from.startsWith(FB_TOPIC_PREFIX)) { // Broadcast topic message
 
                 if (from.endsWith(FB_TOPIC_SURVEY)) {
                     Timber.d("Survey received");
@@ -128,9 +151,59 @@ public class ServerNotification {
                 }
                 mPrefEditor.apply();
 
-            } else { // point-to-point message
-                // TODO: Add point-to-point message processing
+            } else { // No topic - point-to-point message
                 Timber.d("ServerNotification received a point-to-point");
+                if (msgPayload.containsKey(PTP_ALERT_PUBTRANS)) {
+                    if (msgPayload.containsKey(PTP_ALERT_END)) { // Alert expiration time specified
+                        Date alertEndDate = new Date();
+                        try {
+                            alertEndDate = dateTimeFormat.parse(msgPayload.get(PTP_ALERT_END));
+                        } catch (ParseException e) {
+                            Timber.d("ServerNotification failed to parse PTP_ALERT_END: %s", e.getMessage());
+                            e.printStackTrace();
+                        }
+                        long alertEndMillis = alertEndDate.getTime();
+                        long now = System.currentTimeMillis();
+                        if (alertEndMillis > now) {
+                            Timber.d("ServerNotification interpreted remaining alert duration as %d ms.", alertEndMillis - now);
+                            // TODO: Check stored notification status
+                            // Set up an alarm for the alert end time
+                            Context ctx = TrafficSenseApplication.getContext();
+                            AlarmManager alarmManager = (AlarmManager)ctx.getSystemService(Context.ALARM_SERVICE);
+                            Intent intent = new Intent(ctx, TSBootReceiver.class);
+                            intent.setAction(PTP_ALERT_END_ACTION);
+                            intent.putExtra("notification_id", SERVER_NOTIFICATION_ID);
+                            PendingIntent pi = PendingIntent.getBroadcast(ctx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                            alarmManager.set(RTC, alertEndMillis, pi);
+                        }
+                    }
+                    if (msgPayload.containsKey(PTP_ALERT_TYPE)) {
+                        switch (msgPayload.get(PTP_ALERT_TYPE)) {
+                            case "TRAIN":
+                                notificationType = NOTIFICATION_TYPE_TRAIN;
+                                break;
+                            case "TRAM":
+                                notificationType = NOTIFICATION_TYPE_TRAM;
+                                break;
+                            case "SUBWAY":
+                                notificationType = NOTIFICATION_TYPE_SUBWAY;
+                                break;
+                            case "BUS":
+                                notificationType = NOTIFICATION_TYPE_BUS;
+                                break;
+                            case "FERRY":
+                                notificationType = NOTIFICATION_TYPE_FERRY;
+                                break;
+                            default:
+                                notificationType = NOTIFICATION_TYPE_UNKNOWN;
+                                break;
+                        }
+                    }
+
+                } else { // Unknown PTP - do not post
+                    shouldCreateNotification = false;
+                }
+
             }
 
         }
@@ -148,6 +221,21 @@ public class ServerNotification {
                 break;
             case NOTIFICATION_TYPE_BROADCASTMESSAGE:
                 notificationIcon = R.drawable.ic_info_outline_24dp;
+                break;
+            case NOTIFICATION_TYPE_FERRY:
+                notificationIcon = R.drawable.md_activity_ferry_24dp;
+                break;
+            case NOTIFICATION_TYPE_SUBWAY:
+                notificationIcon = R.drawable.md_activity_subway_24dp;
+                break;
+            case NOTIFICATION_TYPE_TRAIN:
+                notificationIcon = R.drawable.md_activity_train_24dp;
+                break;
+            case NOTIFICATION_TYPE_TRAM:
+                notificationIcon = R.drawable.md_activity_subway_24dp;
+                break;
+            case NOTIFICATION_TYPE_BUS:
+                notificationIcon = R.drawable.md_activity_bus_24dp;
                 break;
             default:
                 notificationIcon = R.mipmap.ic_launcher;
--- app/src/main/java/fi/aalto/trafficsense/trafficsense/util/TSBootReceiver.java
+++ app/src/main/java/fi/aalto/trafficsense/trafficsense/util/TSBootReceiver.java
@@ -1,8 +1,10 @@
 package fi.aalto.trafficsense.trafficsense.util;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import fi.aalto.trafficsense.trafficsense.backend.backend_util.ServerNotification;
 import timber.log.Timber;
 
 /**
@@ -13,8 +15,17 @@ public class TSBootReceiver extends BroadcastReceiver
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+        final String action = intent.getAction();
+        if(action.equals(Intent.ACTION_BOOT_COMPLETED)){
             Timber.d("TrafficSense received boot completed.");
+        } else if (action.equals(ServerNotification.PTP_ALERT_END_ACTION)) {
+            int id = intent.getIntExtra("notification_id", -1);
+            if (id != -1) {
+                Timber.d("onReceive cancelling notification: %d", id);
+                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.cancel(id);
+            }
+
         }
     }
 
