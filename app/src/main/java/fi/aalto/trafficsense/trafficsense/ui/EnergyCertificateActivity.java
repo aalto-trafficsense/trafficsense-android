e2274ceec7dac07b066b2162cd50313def2058e3
Mikko Rinne <mikko.rinne@aalto.fi>
2017-01-25 17:02:47 +0200

    First working version of selectable dates for energy certificates.

--- app/src/main/java/fi/aalto/trafficsense/trafficsense/ui/EnergyCertificateActivity.java
+++ app/src/main/java/fi/aalto/trafficsense/trafficsense/ui/EnergyCertificateActivity.java
@@ -1,16 +1,17 @@
 package fi.aalto.trafficsense.trafficsense.ui;
 
 import android.app.ActionBar;
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.*;
 import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGImageView;
 import com.caverock.androidsvg.SVGParseException;
@@ -18,6 +19,7 @@ import com.google.common.base.Optional;
 import fi.aalto.trafficsense.trafficsense.R;
 import fi.aalto.trafficsense.trafficsense.TrafficSenseApplication;
 import fi.aalto.trafficsense.trafficsense.util.BackendStorage;
+import timber.log.Timber;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -25,11 +27,27 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-public class EnergyCertificateActivity extends AppCompatActivity {
+public class EnergyCertificateActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private SVGImageView svgImageView;
     private BackendStorage mStorage;
+    private static Calendar startDate;
+    private static Calendar endDate;
+    private static Calendar currentDate;
+
+    private Button mStartDateButton;
+    private Button mEndDateButton;
+
+    private static int dateDialogId = -1;
+
+    static final int START_DATE_DIALOG = 0;
+    static final int END_DATE_DIALOG = 1;
+    private final SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +62,14 @@ public class EnergyCertificateActivity extends AppCompatActivity {
         svgImageView = new SVGImageView(this);
         container.addView(svgImageView, ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
 
+        mStartDateButton = (Button) findViewById(R.id.energy_button_start);
+        mEndDateButton = (Button) findViewById(R.id.energy_button_end);
+
         mStorage = BackendStorage.create(this);
 
+        // Initialize current date and default start and end to one week
+        currentDate = Calendar.getInstance();
+        setDateOffsets(-7, -1);
     }
 
     @Override
@@ -57,7 +81,126 @@ public class EnergyCertificateActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        fetchCertificate();
+        fetchCertificate(startDate, endDate);
+    }
+
+    private void setDateOffsets(int startOffset, int endOffset) {
+        startDate = (Calendar)currentDate.clone();
+        startDate.add(Calendar.DATE, startOffset);
+        mStartDateButton.setText(DateFormat.getDateInstance().format(startDate.getTime()));
+        endDate = (Calendar)currentDate.clone();
+        endDate.add(Calendar.DATE, endOffset);
+        mEndDateButton.setText(DateFormat.getDateInstance().format(endDate.getTime()));
+    }
+
+    public void selectWeek(View view) { // Select the last week
+        Timber.d("Week selected");
+        setDateOffsets(-7, -1);
+        fetchCertificate(startDate, endDate);
+    }
+
+    public void selectMonth(View view) { // Select the last month
+        Timber.d("Month selected");
+        startDate = (Calendar)currentDate.clone();
+        startDate.add(Calendar.MONTH, -1);
+        mStartDateButton.setText(DateFormat.getDateInstance().format(startDate.getTime()));
+        endDate = (Calendar)currentDate.clone();
+        endDate.add(Calendar.DATE, -1);
+        mEndDateButton.setText(DateFormat.getDateInstance().format(endDate.getTime()));
+        fetchCertificate(startDate, endDate);
+    }
+
+    public void selectYear(View view) { // Select the last year
+        Timber.d("Year selected");
+        startDate = (Calendar)currentDate.clone();
+        startDate.add(Calendar.YEAR, -1);
+        mStartDateButton.setText(DateFormat.getDateInstance().format(startDate.getTime()));
+        endDate = (Calendar)currentDate.clone();
+        endDate.add(Calendar.DATE, -1);
+        mEndDateButton.setText(DateFormat.getDateInstance().format(endDate.getTime()));
+        fetchCertificate(startDate, endDate);
+    }
+
+    public void startDateClick(View view) { // Select start date
+        Timber.d("Start date selection selected");
+        DialogFragment newFragment = new StartDatePicker();
+        newFragment.show(getSupportFragmentManager(), "startPicker");
+    }
+
+    public void endDateClick(View view) { // Select end date
+        Timber.d("End date selection selected");
+        DialogFragment newFragment = new EndDatePicker();
+        newFragment.show(getSupportFragmentManager(), "endPicker");
+    }
+
+    public static class StartDatePicker extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            dateDialogId = START_DATE_DIALOG;
+            // Use startDate as the default date in the picker
+            int year = startDate.get(Calendar.YEAR);
+            int month = startDate.get(Calendar.MONTH);
+            int day = startDate.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+            // Set maximum to current date
+            DatePicker dp = dpd.getDatePicker();
+            dp.setMaxDate(currentDate.getTimeInMillis());
+            return dpd;
+        }
+    }
+
+    public static class EndDatePicker extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            dateDialogId = END_DATE_DIALOG;
+            // Use endDate as the default date in the picker
+            int year = endDate.get(Calendar.YEAR);
+            int month = endDate.get(Calendar.MONTH);
+            int day = endDate.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+            // Set maximum to current date, minimum to start date
+            DatePicker dp = dpd.getDatePicker();
+            dp.setMaxDate(currentDate.getTimeInMillis());
+            dp.setMinDate(startDate.getTimeInMillis());
+            return dpd;
+        }
+    }
+
+    @Override
+    public void onDateSet (DatePicker view, int year, int month, int day) {
+        switch (dateDialogId) {
+            case START_DATE_DIALOG:
+                startDate.set(Calendar.YEAR, year);
+                startDate.set(Calendar.MONTH, month);
+                startDate.set(Calendar.DAY_OF_MONTH, day);
+                if (startDate.after(endDate)) {
+                    // Move endDate default equal to startDate
+                    endDate = (Calendar)startDate.clone();
+                    mEndDateButton.setText(DateFormat.getDateInstance().format(endDate.getTime()));
+                }
+                mStartDateButton.setText(DateFormat.getDateInstance().format(startDate.getTime()));
+                break;
+            case END_DATE_DIALOG:
+                endDate.set(Calendar.YEAR, year);
+                endDate.set(Calendar.MONTH, month);
+                endDate.set(Calendar.DAY_OF_MONTH, day);
+                if (startDate.after(endDate)) {
+                    // Should not happen, but fix here in case
+                    endDate = (Calendar)startDate.clone();
+                }
+                mEndDateButton.setText(DateFormat.getDateInstance().format(endDate.getTime()));
+                break;
+            default:
+                // Huh??
+        }
+        fetchCertificate(startDate, endDate);
+
     }
 
     @Override
@@ -66,14 +209,13 @@ public class EnergyCertificateActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
     }
 
-
-    private void fetchCertificate() {
+    private void fetchCertificate(Calendar start, Calendar end) {
         Optional<String> token = mStorage.readSessionToken();
         if (!token.isPresent()) {
             Toast.makeText(this, R.string.not_signed_in, Toast.LENGTH_SHORT).show();
         } else {
             try {
-                URL url = new URL(mStorage.getServerName().toString() + "/svg/" + token.get());
+                URL url = new URL(mStorage.getServerName() + "/svg/" + token.get() + "?firstday=" + mDateFormat.format(start.getTime()) + "&lastday=" + mDateFormat.format(end.getTime()));
                 DownloadDataTask downloader = new DownloadDataTask();
                 downloader.execute(url);
             } catch (MalformedURLException e) {
@@ -116,16 +258,18 @@ public class EnergyCertificateActivity extends AppCompatActivity {
         }
 
         protected void onPostExecute(String info) {
-            SVG svgImage;
-            try {
-                svgImage = SVG.getFromString(info);
-            } catch(SVGParseException e) {
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT);
-                toast.show();
-                return;
+            if (info.length() > 20) {
+                SVG svgImage;
+                try {
+                    svgImage = SVG.getFromString(info);
+                } catch(SVGParseException e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                svgImageView.setSVG(svgImage);
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.energy_load_error, Toast.LENGTH_LONG).show();
             }
-            svgImageView.setSVG(svgImage);
         }
     }
 }
